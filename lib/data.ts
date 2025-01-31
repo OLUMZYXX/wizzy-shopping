693a181d2022ffa8effa659a04c92687d7c4b5e7
OLUMZYXX <akinwumiolumide5@gmail.com>
2025-01-31 09:46:27 +0100

    04-create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,4 +33,27 @@ export const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      images: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Seller in T-shirts',
+      buttonCaption: 'Shop Now',
+      images: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      images: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
 }
